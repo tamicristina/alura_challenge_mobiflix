83634deed7e3687e6f50bb056c3e02d0d761b539
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-03-02 21:02:21 -0300

    create home page structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import theme from "./src/theme";
+import { Home } from "./src/screens/Home";
+import { ThemeProvider } from "styled-components";
+import { useFonts, BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
+import { ActivityIndicator } from "react-native/Libraries/Components/ActivityIndicator/ActivityIndicator";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ BebasNeue_400Regular });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
