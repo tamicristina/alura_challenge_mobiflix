227ee16a3c0175b260b460a7acd34a531baaa7d0
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-03-02 21:01:39 -0300

    create theme file

---
+++ src/theme/index.ts
@@ -0,0 +1,18 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    RED: "#D82D2D",
+    BLACK: "#222222",
+    BLUE: "#2478DF",
+  },
+  FONT_FAMILY: {
+    REGULAR: "BebasNeue_400Regular",
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+};
