b14e3ec868decef6b610432a8dd489fe0012be93
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-03-06 20:46:23 -0300

    add style to status bar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import { ThemeProvider } from "styled-components";
+import { ActivityIndicator, StatusBar } from "react-native";
+import { BebasNeue_400Regular, useFonts } from "@expo-google-fonts/bebas-neue";
+
+
 import theme from "./src/theme";
 import { Home } from "./src/screens/Home";
-import { ThemeProvider } from "styled-components";
-import { useFonts, BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
-import { ActivityIndicator } from "react-native/Libraries/Components/ActivityIndicator/ActivityIndicator";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ BebasNeue_400Regular });
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Home /> : <ActivityIndicator />}
     </ThemeProvider>
   );
