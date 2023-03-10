8742c016b7a895252f61690b31154cad4b0863b3
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-03-09 21:20:05 -0300

    add category list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { ImageBackground, Text } from "react-native";
+import { ImageBackground, ScrollView, Text } from "react-native";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Tag } from "../../components/Tag";
+import { categories } from "../../services/dados";
 import { ButtonContainer, Container } from "./styles";
 
 export function Home() {
   const image = { uri: "https://img.youtube.com/vi/N546dnt2qxc/hqdefault.jpg" };
+
+  const topCategoriesHighlights = categories[0].categories
+ 
   return (
     <Container>
       <Header title="MOBIFLIX" />
@@ -23,6 +28,9 @@ export function Home() {
           <Button label="Assista agora" />
         </ButtonContainer>
       </ImageBackground>
+      <ScrollView>
+        {topCategoriesHighlights.map((category) => (<Tag label={category}/>))}
+      </ScrollView>
     </Container>
   );
 }
---
+++ src/services/dados.ts
@@ -0,0 +1,39 @@
+export type ICategoriesTypes = {
+  nome: string;
+  image: string;
+  video: string;
+  categories: string[];
+};
+
+export const categories: ICategoriesTypes[] = [
+  {
+    nome: "Maggi",
+    image: "https://img.youtube.com/vi/N546dnt2qxc/hqdefault.jpg",
+    video: "https://img.youtube.com/vi/N546dnt2qxc/hqdefault.jpg",
+    categories: ["Aventura", "Ação", "Magia"],
+  },
+  {
+    nome: "Naruto",
+    image: "https://img.youtube.com/vi/N546dnt2qxc/fieqge7aYJM.jpg",
+    video: "https://img.youtube.com/vi/N546dnt2qxc/fieqge7aYJM.jpg",
+    categories: ["Aventura", "Ação"],
+  },
+  {
+    nome: "Spy x Family",
+    image: "https://img.youtube.com/vi/N546dnt2qxc/rYmgN3eQxVs.jpg",
+    video: "https://img.youtube.com/vi/N546dnt2qxc/rYmgN3eQxVs.jpg",
+    categories: ["Aventura", "Ação", "Family"],
+  },
+  {
+    nome: "Kimetsu No Yaba",
+    image: "https://img.youtube.com/vi/N546dnt2qxc/5adX1Bni4L4.jpg",
+    video: "https://img.youtube.com/vi/N546dnt2qxc/5adX1Bni4L4.jpg",
+    categories: ["Aventura", "Ação", "Magia","Terror"],
+  },
+  {
+    nome: "Kimetsu No Yaba",
+    image: "https://img.youtube.com/vi/N546dnt2qxc/rJzmAzQp6pk.jpg",
+    video: "https://img.youtube.com/vi/N546dnt2qxc/rJzmAzQp6pk.jpg",
+    categories: ["Aventura", "Ação", "Magia","Terror"],
+  },
+];
