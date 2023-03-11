7b7e64b62b513b97c2b7e5322baca18d0fefa2e9
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-03-10 21:16:30 -0300

    create type colors

---
+++ src/components/Tag/types.ts
@@ -0,0 +1,34 @@
+export type ICategoriesColors = {
+  category: string;
+  color: string;
+};
+
+export const categoriesColors: ICategoriesColors[] = [
+  {
+    category:'Aventura',
+    color: "#2478DF"
+  }
+,
+  {
+    category:'Ação',
+    color: "#19940F"
+  }
+,
+  {
+    category:'Magia',
+    color: "#2478DF"
+  }
+,
+  {
+    category:'Family',
+    color: "#7B61FF"
+  }
+,
+  {
+    category:'Terror',
+    color: "#D82D2D"
+  }
+,
+
+]
+
